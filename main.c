fb320af2c0bb0fd9b187f8c2ef425835b434f2cc
Ilham3040 <ilhamdidazakaria@gmail.com>
2026-09-20 18:34:45 +0700

    More proper parser lexer flow

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include "file_loader.h"
 #include "parser.h"
--- parser.c
+++ parser.c
@@ -1,3 +1,4 @@
+#include "parser.h"
 #include "bool.h"
 #include "file_loader.h"
 #include "status.h"
@@ -8,6 +9,19 @@
 #include "char_manip.h"
 #include "string_dynamic_array.h"
 
+typedef enum Token {
+    TOKEN_FIELDS,
+    TOKEN_COMMA,
+    TOKEN_SEMICOLON,
+    TOKEN_ASSIGNMENT,
+    TOKEN_DOUBLE_QUOTE,
+    TOKEN_OPEN_BRACKET,
+    TOKEN_CLOSE_BRACKET,
+    TOKEN_OPEN_CURLY_BRACKET,
+    TOKEN_CLOSE_CURLY_BRACKET,
+    TOKEN_STRING_LITERAL,
+    TOKEN_UNKNOWN
+}Token;
 
 typedef enum GenerateFields {
     STATUS_HEADER,
@@ -29,15 +43,43 @@ typedef enum TypeMode {
 } TypeMode;
 
 TypeMode type_check(const char target) {
-    if (target == 34) {
-        return STRING_LITERAL;
-    }
-    else if (is_alphabet_numeric(target) || target == 95 || target == 46 || target == 47) {
+    if (is_alphabet_numeric(target) || target == 95 ||target == 46 ||target == 47) {
         return ALPHANUMERIC;
     } else if (is_whitespace(target)) {
         return WHITESPACE;
     } else {
         return SYMBOL;
+    }
+}
+
+Token check_token_enumerator(const char target) {
+    switch (target) {
+        case '=':
+            return TOKEN_ASSIGNMENT;
+
+        case ';':
+            return TOKEN_SEMICOLON;
+
+        case '"':
+            return TOKEN_DOUBLE_QUOTE;
+
+        case '(':
+            return TOKEN_OPEN_BRACKET;
+
+        case ')':
+            return TOKEN_CLOSE_BRACKET;
+
+        case '{':
+            return TOKEN_OPEN_CURLY_BRACKET;
+
+        case '}':
+            return TOKEN_CLOSE_CURLY_BRACKET;
+
+        case ',':
+            return TOKEN_COMMA;
+
+        default:
+            return TOKEN_UNKNOWN;
     }
 }
 
@@ -47,6 +89,8 @@ static TypeMode head_mode, tail_mode = WHITESPACE;
 static FileString *file_string = NULL;
 static size_t line_scanned = 1;
 static LookupStatus lookup_status = {NO_ERROR, 5};
+
+
 static StringDynamicArray *string_dynamic_array;
 static StringDynamicArrayPointer status_header;
 static StringDynamicArrayPointer utils_dir;
@@ -67,10 +111,16 @@ void set_input_filename(StringDynamicArrayPointer *pointer) {
     memcpy(&input_file_name, pointer, sizeof(StringDynamicArrayPointer));
 }
 
+void reset_tail_mode() {
+    tail = head;
+    tail_mode = type_check(file_string->start[tail]);
+    head++;
+}
 
 
-void lexer_linear_scan() {
+Token lexer_linear_scan() {
     const struct KeyMapping *match;
+    Token token_scanned = TOKEN_UNKNOWN;
 
     while (head < file_string->length) {
         head_mode = type_check(file_string->start[head]);
@@ -79,30 +129,38 @@ void lexer_linear_scan() {
             line_scanned++;
         }
 
+        if (tail_mode == STRING_LITERAL && file_string->start[head] != '"' && file_string->start[head] != ';' &&file_string->start[head] != '\n') {
+            head_mode = STRING_LITERAL;
+        }
+
         if (head_mode != tail_mode) {
 
             switch (tail_mode) {
                 case WHITESPACE:
-                    tail = head;
-                    tail_mode = type_check(file_string->start[tail]);
-                    head++;
+                    reset_tail_mode();
                     continue;
 
                 case ALPHANUMERIC:
                     match = lookup_key(file_string->start + tail, head-tail);
                     if (match == NULL) {
                         lookup_status.status = FIELD_MATCH_NOT_FOUND;
-                        return;
+                        return TOKEN_UNKNOWN;
                     }
                     lookup_status.fields_id = match->id;
-                    return;
+                    return TOKEN_FIELDS;
 
                 case SYMBOL:
-                    break;
+                    return check_token_enumerator(file_string->start[tail]);
+
 
                 case STRING_LITERAL:
-                    break;
-
+                    if (file_string->start[head] == ';' ||file_string->start[head] == '\n') {
+                        return TOKEN_UNKNOWN;
+                    }
+                    if (file_string->start[head] == '"') {
+                        return TOKEN_STRING_LITERAL;
+                    }
+                    continue;
             }
         }
 
@@ -110,33 +168,8 @@ void lexer_linear_scan() {
     }
 }
 
-void reset_tail_mode() {
-    tail = head;
-    tail_mode = type_check(file_string->start[tail]);
-    head++;
-}
 
-Status move_head_forward_until(const char target) {
-    size_t initial_head_index = head;
-    while (head < file_string->length) {
-        if (file_string->start[head] == target) {
-            return ITEM_FOUND;
-        }
-        head++;
-    }
-    return ITEM_NOT_FOUND;
-}
 
-Status move_tail_backward_until(const char target) {
-    size_t initial_head_index = head;
-    while (tail > 0) {
-        if (file_string->start[tail] == target) {
-            return ITEM_FOUND;
-        }
-        tail--;
-    }
-    return ITEM_NOT_FOUND;
-}
 
 void get_one_line(){
     while (head < file_string->length) {
@@ -164,6 +197,73 @@ void get_one_line_error_print() {
 }
 
 Status parser_start() {
+    size_t temp_head = 0;
+    Status scan_status = ITEM_FOUND;
+    Token current_token = TOKEN_UNKNOWN;
+
+    current_token = lexer_linear_scan();
+
+    if (current_token != TOKEN_FIELDS) {
+        printf("Field {%.*s} match up not found at:\n", head-tail,file_string->start+tail);
+        get_one_line_error_print();
+        return lookup_status.status;
+    }
+
+    if (lookup_status.fields_id != STATUS_HEADER) {
+        printf("Wrong fields order {%.*s} at:\n",  head-tail,file_string->start+tail);
+        get_one_line_error_print();
+        printf("\nThe order should be:\n status_header, utils_dir, header_dir, then struct_template");
+        return MISMATCH_EXPECTATION;
+    }
+
+    reset_tail_mode();
+    current_token = lexer_linear_scan();
+
+
+    if (current_token != TOKEN_ASSIGNMENT) {
+        printf("Missing assignment at:\n");
+        get_one_line_error_print();
+        printf("\nIt should be: \n status_header = \"{file_path}\"; \n");
+        return MISMATCH_EXPECTATION;
+    }
+
+    reset_tail_mode();
+    current_token = lexer_linear_scan();
+
+    if (current_token != TOKEN_DOUBLE_QUOTE) {
+        printf("Missing double quote at:\n");
+        get_one_line_error_print();
+        printf("\nIt should be: \n status_header = \"{file_path}\"; \n");
+        return MISMATCH_EXPECTATION;
+    }
+
+    reset_tail_mode();
+    tail_mode = STRING_LITERAL;
+    current_token = lexer_linear_scan();
+
+    if (current_token != TOKEN_STRING_LITERAL) {
+        printf("Expected string literal at:\n");
+        get_one_line_error_print();
+        printf("\nIt should be: \n status_header = \"{file_path}\"; \n");
+        return MISMATCH_EXPECTATION;
+    }
+
+    reset_tail_mode();
+    current_token = lexer_linear_scan();
+
+    if (current_token != TOKEN_SEMICOLON) {
+        printf("Missing semicolon at:\n");
+        get_one_line_error_print();
+        printf("\nIt should be: \n status_header = \"{file_path}\"; \n");
+        return MISMATCH_EXPECTATION;
+    }
+
+
+
+    return NO_ERROR;
+}
+
+Status parser_start_old() {
     size_t temp_head = 0;
     Status scan_status = ITEM_FOUND;
 
